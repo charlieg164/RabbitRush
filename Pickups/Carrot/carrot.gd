8cf71e119dcd6a3021157734d789e5590775282f
charlieg164 <163777831+charlieg164@users.noreply.github.com>
2024-03-17 21:32:41 +0000

    Add files via upload

---
+++ Pickups/Carrot/carrot.gd
@@ -0,0 +1,18 @@
+extends Area2D
+
+var carrot = 0
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	carrot = 0
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
+
+
+func _on_area_2d_body_entered(body):
+	carrot = carrot + 1
+	queue_free()
+	print(carrot)
